929a76b431269d64d04a42327567ab59eb2870ef
Fa <aifa.akmal.rashid@gmail.com>
2017-11-10 23:19:21 +0800

    mt first change

--- app/src/main/java/aifa/spadesphinx/Home.java
+++ app/src/main/java/aifa/spadesphinx/Home.java
@@ -4,9 +4,8 @@ import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
-import android.view.ViewConfiguration;
 import android.widget.Button;
-import android.widget.TextView;
+
 
 public class Home extends AppCompatActivity implements View.OnClickListener{
 
